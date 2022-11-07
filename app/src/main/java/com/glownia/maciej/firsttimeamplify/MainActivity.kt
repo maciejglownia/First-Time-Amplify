36a27675432790b9f9ec21372bbbefff9f99f740
Maciej Glownia <maciej.glownia@infor.com>
2022-11-07 14:16:43 +0100

    Add onActivityResult method to receive the web direct after authentication

--- app/src/main/java/com/glownia/maciej/firsttimeamplify/MainActivity.kt
+++ app/src/main/java/com/glownia/maciej/firsttimeamplify/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.glownia.maciej.firsttimeamplify
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.glownia.maciej.firsttimeamplify.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -54,5 +54,11 @@ class MainActivity : AppCompatActivity() {
                 Backend.signIn(this)
             }
         }
+    }
+
+    // receive the web redirect after authentication
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Backend.handleWebUISignInResponse(requestCode, resultCode, data)
     }
 }
